e1c9d27149609899e2da201567bed220547508e1
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-02-06 13:24:49 +0100

    new buttons in main section

--- src/components/button_link.tsx
+++ src/components/button_link.tsx
@@ -3,14 +3,16 @@ import "../styles/button.scss";
 interface ButtonProps {
     text: string;
     icon: string;
-    link: string;
+    link: string
+    icon_fake: string;
 }
 
-const Button = ({ text, icon, link }: ButtonProps) => {
+const Button = ({ text, icon, link, icon_fake }: ButtonProps) => {
     return (
         <a href={link} className="button" target="_blank" rel="noopener noreferrer">
             <span className="icon">{icon}</span>
             {text}
+            <span className="icon">{icon_fake}</span>
         </a>
     );
 };
--- src/sites/Main.tsx
+++ src/sites/Main.tsx
@@ -4,9 +4,9 @@ import "../styles/Main.scss";
 const Main = () => {
     return (
         <main className="main">
-            <Button text="Portfolio" icon="🌍" link="#" />
-            <Button text="GitHub" icon="🐙" link="#" />
-            <Button text="LinkedIn" icon="🔗" link="#" />
+            <Button text="Portfolio" icon="🌍" link="https://kolegatatar.github.io/Portfolio/" />
+            <Button text="GitHub" icon="🐙" link="https://github.com/KolegaTatar" />
+            <Button text="LinkedIn" icon="🔗" link="https://www.linkedin.com/in/wiktor-tatarynowicz/" />
             <Button text="E-Mail" icon="📧" link="mailto:test@example.com" />
         </main>
     );
