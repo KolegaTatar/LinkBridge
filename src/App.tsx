e780baff258e9932fe0358cc80902c822ad49136
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-02-10 22:34:02 +0100

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./index.scss";
 import Main from "./sites/Main.tsx";
+import Header from "./sites/Header.tsx";
 
 
 function App(){
   return (
     <div className="container">
+        <Header />
         <Main/>
     </div>
   );
